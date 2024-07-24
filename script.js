1cd9654e119c8bc390b34775db7f28c3189e37c0
Bora-Di <mbou7841@gmail.com>
2024-07-25 00:08:15 +0100

    filter

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-filterSelection("all");
+window.onload = function() {
+    filterSelection("all");
+}
 
-function filterSelection(c){
-    var x,y,i,j;
-    x= document.getElementsByClassName("image");
-    y= document.getElementsByClassName("tab");
+function filterSelection(c) {
+    var x, y, i, j;
+    x = document.getElementsByClassName("image");
+    y = document.getElementsByClassName("tab");
 
-    for(j = 0; j < y.length; j++){
-
-        RemoveClass( y[j], "active");
-        if(y[j].className.indexOf(c) > -1) {
-        AddClass(y[j], "active");
+    for (j = 0; j < y.length; j++) {
+        RemoveClass(y[j], "active");
+        if (y[j].className.indexOf(c) > -1) {
+            AddClass(y[j], "active");
         }
     }
 
-    if(c == "all"){
-        c="";
+    if (c == "all") {
+        c = "";
     }
 
-    for(i = 0; i < x.length; i++){
-
-        RemoveClass( x[i], "show");
-        if(x[i ].className.indexOf(c)>-1){
-        AddClass(x[i],"show");
+    for (i = 0; i < x.length; i++) {
+        RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) {
+            AddClass(x[i], "show");
         }
     }
 }
 
-function AddClass (element,name){
-    element.ClassList.add(name);
+function AddClass(element, name) {
+    element.classList.add(name);
 }
 
-function RemoveClass (element,name){
-    element.ClassList.remove(name);
+function RemoveClass(element, name) {
+    element.classList.remove(name);
 }
-
